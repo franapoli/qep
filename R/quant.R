7e9f5845ec8140c31fbaad5393c3b7fe952310bd
Francesco Napolitano <franapoli@gmail.com>
2016-05-13 00:30:32 +0200

    adjusted logistic.ep parameters

--- R/quant.R
+++ R/quant.R
@@ -1,45 +1,44 @@
 
 logistic.ep <- function(nbins, steep, width, baseline, zero, trim=T)
 {
-  ##l1 <- 2^(10*steep);
-     #hsh <- 60*(width);
-  l1 <- steep; hsh <- width
-  vsh <- baseline; rot <- zero
+    l1 <-  30*steep;
+    hsh <- 30*width;
+    vsh <- baseline; rot <- zero
   
-  odd <- nbins%%2
-  nbins <- ceiling(nbins/2)
-  f <- plogis(seq(-l1,l1,length=nbins)+hsh)+vsh;
-  f <- (1-vsh)/(max(f)-min(f))*(f-max(f))+1 # range normalization
+    odd <- nbins%%2
+    nbins <- ceiling(nbins/2)
+    f <- plogis(seq(-l1,l1,length=nbins)+hsh)+vsh;
+    f <- (1-vsh)/(max(f)-min(f))*(f-max(f))+1 # range normalization
 
-  f2 <- f[length(f):1]
+    f2 <- f[length(f):1]
 
-  if(odd)
-    f <- c(f[1:(length(f)-1)],f2) else f <- c(f,f2)
+    if(odd)
+        f <- c(f[1:(length(f)-1)],f2) else f <- c(f,f2)
 
-  shiftf <- function(f, rot) {
-    
-    l <- length(f)
-    if(!trim) {
-      f <- f[rot:(l+rot-1) %% l + 1]
-    } else {
-      if(rot>0) {
-        f <- c(rep(f[1], rot), f)[1:length(f)]
-      } else {
-        f <- tail(c(f, rep(tail(f,1), abs(rot))), length(f))
-      }
-      f <- f/sum(f)
+    shiftf <- function(f, rot) {
+        
+        l <- length(f)
+        if(!trim) {
+            f <- f[rot:(l+rot-1) %% l + 1]
+        } else {
+            if(rot>0) {
+                f <- c(rep(f[1], rot), f)[1:length(f)]
+            } else {
+                f <- tail(c(f, rep(tail(f,1), abs(rot))), length(f))
+            }
+            f <- f/sum(f)
+        }
+        return(f)
     }
+
+    if(length(zero)==1)
+        {
+            f <- shiftf(f,rot)
+        } else {
+            f <- sapply(round(rot*nbins), shiftf, f=f)
+        }
+
     return(f)
-}
-
-  if(length(zero)==1)
-    {
-        f <- shiftf(f,rot)
-    } else {
-      f <- sapply(round(rot*nbins), shiftf, f=f)
-    }
-
-  return(f)
 }
                           
 quantize.ep <- function(data, scheme,
