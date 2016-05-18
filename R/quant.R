5bb75cbee3b57291c2f5e7d6913efa9af2f9a621
Ciccio <f.napolitano@tigem.it>
2016-05-18 17:54:30 +0200

    added norm and uniform q schemes

--- R/quant.R
+++ R/quant.R
@@ -1,8 +1,50 @@
 
-logistic.ep <- function(nbins, steep, width, baseline, zero, trim=T)
+shiftf <- function(f, zero, trim=T) {
+
+    doit <- function(f, rot, trim) {
+        l <- length(f)
+        if(!trim) {
+            f <- f[rot:(l+rot-1) %% l + 1]
+        } else {
+            if(rot>0) {
+                f <- c(rep(f[1], rot), f)[1:length(f)]
+            } else {
+                f <- tail(c(f, rep(tail(f,1), abs(rot))), length(f))
+            }
+            f <- f/sum(f)
+        }
+        return(f)
+    }
+
+  if(length(zero)==1)
+    {
+        f <- doit(f,zero,trim)
+    } else {
+      f <- sapply(round(zero*nbins), shiftf, f=f, trim=trim)
+    }
+    
+    return(f)
+}
+
+
+unif.qs <- function(nbins)
 {
-  ##l1 <- 2^(10*steep);
-     #hsh <- 60*(width);
+    uni <- seq(0,1,length=nbins)
+    return(uni)
+}
+
+
+norm.qs <- function(nbins, sd, baseline, zero, trim=T)
+{
+    vsh <- baseline
+    uni <- seq(0,1,length=nbins)
+    f <- dnorm(seq(-1,1,length=31), sd=sd)
+    f <- (1-vsh)/(max(f)-min(f))*(f-max(f))+1 # range normalization
+    f <- shiftf(f,zero,trim)
+}
+
+logis.qs <- function(nbins, steep, width, baseline, zero, trim=T)
+{
   l1 <- steep; hsh <- width
   vsh <- baseline; rot <- zero
   
@@ -13,39 +55,18 @@ logistic.ep <- function(nbins, steep, width, baseline, zero, trim=T)
 
   f2 <- f[length(f):1]
 
-  if(odd)
-    f <- c(f[1:(length(f)-1)],f2) else f <- c(f,f2)
+  if(odd) {
+      f <- c(f[1:(length(f)-1)],f2)
+  } else f <- c(f,f2)
 
-  shiftf <- function(f, rot) {
-    
-    l <- length(f)
-    if(!trim) {
-      f <- f[rot:(l+rot-1) %% l + 1]
-    } else {
-      if(rot>0) {
-        f <- c(rep(f[1], rot), f)[1:length(f)]
-      } else {
-        f <- tail(c(f, rep(tail(f,1), abs(rot))), length(f))
-      }
-      f <- f/sum(f)
-    }
-    return(f)
-}
-
-  if(length(zero)==1)
-    {
-        f <- shiftf(f,rot)
-    } else {
-      f <- sapply(round(rot*nbins), shiftf, f=f)
-    }
+  f <- shiftf(f,zero,trim)
 
   return(f)
 }
                           
-quantize.ep <- function(data, scheme,
-                        decreasing=T, verbose=T)
+quantize <- function(data, qscheme,
+                     decreasing=T, verbose=T)
 {
-    qep_checknames(data)
     
     if(decreasing) {
         data <- apply(-data,2,rank,ties.method="random")
@@ -54,20 +75,20 @@ quantize.ep <- function(data, scheme,
     data <- (data-1)/(nrow(data)-1)
 
 
-    if(!is.null(dim(scheme))) {
-        if(abs(sum(scheme)-ncol(scheme))>(10^-15)*ncol(scheme))
+    if(!is.null(dim(qscheme))) {
+        if(abs(sum(qscheme)-ncol(qscheme))>(10^-15)*ncol(qscheme))
             stop("All schemes must sum to 1")
-        if(ncol(scheme)!=ncol(data))
+        if(ncol(qscheme)!=ncol(data))
             stop("data and scheme must have same number of columns")
         binned <- matrix(NA,nrow(data),ncol(data))
         for(i in 1:ncol(data)) {
-          cs <- cumsum(scheme[,i])
+          cs <- cumsum(qscheme[,i])
           ## make sure 1 is 1 and not 1-10^16...    
           cs[length(cs)] <- 1
           binned[,i] <- cut(data[,i], c(0,cs), F, T)
         }
     } else {
-      cs <- cumsum(scheme)
+      cs <- cumsum(qscheme)
       ## make sure 1 is 1 and not 1-10^16...    
       cs[length(cs)] <- 1
       binned <- apply(data, 2, cut, c(0,cs), F, T)
